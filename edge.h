f3cd349f03b08e316ecbb070395fc837988e9156
Natalia Ksenz <velvet@MacBook-Pro.home>
2022-05-11 09:14:17 -0700

    Initial commit

---
+++ edge.h
@@ -0,0 +1,18 @@
+/***************************************************************************
+Name: Natalia Ksenz
+Final Project
+Purpose: Create a working bi-directional weighted graph class with all the
+standard methods for a data structure of that type.
+
+File: edge.h
+***************************************************************************/
+
+#ifndef GRAPH_EDGE_H
+#define GRAPH_EDGE_H
+
+struct Edge {
+    int destinationVertexID;
+    int weight;
+};
+
+#endif // GRAPH_EDGE_H
