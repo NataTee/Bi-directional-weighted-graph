d77a968585cb74604ee42ef01998c2b80aa7c8e3
Natalia Ksenz <velvet@MacBook-Pro.home>
2022-05-11 20:53:33 -0700

    Initial commit

---
+++ main.h
@@ -0,0 +1,25 @@
+/***************************************************************************
+Name: Natalia Ksenz
+Final Project
+Purpose: Create a working bi-directional weighted graph class with all the
+standard methods for a data structure of that type.
+
+File: main.h
+***************************************************************************/
+
+#ifndef GRAPH_MAIN_H
+#define GRAPH_MAIN_H
+
+#include <iostream> // cout, endl
+#include <stdlib.h> // srand
+#include <time.h>   // time
+#include <string>   // string
+#include "graph.h"
+
+#define TESTDATA1 5
+ 
+using std::cout;
+using std::endl;
+using std::string;
+
+#endif //GRAPH_MAIN_H
